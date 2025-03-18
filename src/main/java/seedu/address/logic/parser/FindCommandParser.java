40f05bf6e56f866bbc746a526d919894dca530a0
GCheeYang <>
2025-03-18 11:07:45 +0800

    Find command by phone number

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,12 +1,19 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_PHONE;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
+import seedu.address.model.person.PhoneNumberContainsKeywordsPredicate;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -25,9 +32,29 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        if (trimmedArgs.startsWith("/p")) {
+            // Search by phone number
+            String phoneKeywords = trimmedArgs.substring(2).trim();
+            if (phoneKeywords.isEmpty()) {
+                throw new ParseException(
+                        String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+            }
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+            List<String> keywords = Arrays.asList(phoneKeywords.split("\\s+"));
+            for (String keyword : keywords) {
+                if (!Phone.isValidPhone(keyword)) {
+                    throw new ParseException(MESSAGE_INVALID_PHONE);
+                }
+            }
+
+            Predicate<Person> predicate = new PhoneNumberContainsKeywordsPredicate(keywords);
+            return new FindCommand(predicate);
+        } else {
+            // Search by name
+            List<String> keywords = Arrays.asList(trimmedArgs.split("\\s+"));
+            Predicate<Person> predicate = new NameContainsKeywordsPredicate(keywords);
+            return new FindCommand(predicate);
+        }
     }
 
 }
