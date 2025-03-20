17042468759e09f3411bb310ef76ecad29af45a0
Parrycys <parrychan339@yahoo.com>
2025-03-21 00:18:44 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/commands/ImportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ImportCommandTest.java
@@ -20,7 +20,6 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
-import seedu.address.testutil.Assert;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
