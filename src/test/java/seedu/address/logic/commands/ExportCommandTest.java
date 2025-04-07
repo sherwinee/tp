d0125335f2e096c7037ce9e064ec89d720043f53
Sherwin Ee <sherwinee@gmail.com>
2025-04-08 02:35:15 +0800

    Add tests

--- src/test/java/seedu/address/logic/commands/ExportCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ExportCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.ExportCommand.MESSAGE_EXPORT_FAILURE;
+import static seedu.address.logic.commands.ExportCommand.MESSAGE_EXPORT_SUCCESS_CSV;
 import static seedu.address.logic.commands.ExportCommand.MESSAGE_EXPORT_SUCCESS_VCF;
 import static seedu.address.logic.commands.ExportCommand.MESSAGE_FILE_EXISTS;
 import static seedu.address.logic.commands.ExportCommand.MESSAGE_NO_CONTACTS;
@@ -15,8 +16,6 @@ import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.commons.util.FileUtil;
-import seedu.address.commons.util.FileUtilTest;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -38,7 +37,7 @@ public class ExportCommandTest {
     }
 
     @Test
-    public void execute_typicalAddressBook_populatedFile() {
+    public void execute_typicalAddressBookVcf_populatedFile() {
         model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
         String filename = "typicalTest.vcf";
@@ -50,7 +49,23 @@ public class ExportCommandTest {
         ec = new ExportCommand(filename);
         expectedMsg = String.format(MESSAGE_EXPORT_FAILURE, filename, MESSAGE_FILE_EXISTS);
         assertCommandFailure(ec, model, expectedMsg);
-        new File(filename).delete();
+        new File(getAbsoluteExportFilePath(filename)).delete();
+    }
+
+    @Test
+    public void execute_typicalAddressBookCsv_populatedFile() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        String filename = "typicalTest.csv";
+        ExportCommand ec = new ExportCommand(filename);
+        String expectedMsg = String.format(MESSAGE_EXPORT_SUCCESS_CSV, getAbsoluteExportFilePath(filename));
+        assertCommandSuccess(ec, model, expectedMsg, expectedModel);
+
+        // Test for same filename failure
+        ec = new ExportCommand(filename);
+        expectedMsg = String.format(MESSAGE_EXPORT_FAILURE, filename, MESSAGE_FILE_EXISTS);
+        assertCommandFailure(ec, model, expectedMsg);
+        new File(getAbsoluteExportFilePath(filename)).delete();
     }
 
     @Test
